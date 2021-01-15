1efa666e7b10b70a879cb192f6e6fd6933e5a2af
Antonio Neves <antonio.carlos.cosme.boturao@gmail.com>
2021-01-15 14:21:26 +0000

    15/01/2021

--- projeto/models/connection.js
+++ projeto/models/connection.js
@@ -1,14 +1,35 @@
-var mysql = require('mysql');
-var util = require('util');
+const mysql = require('mysql2');
+const util = require('util');
 
-var pool = mysql.createPool({
-    connectionLimit:20,
-    host: 'localhost',
+const pool = mysql.createPool({
+    connectionLimit:10,
+    host:'localhost',
     user:'root',
-    password:'Antonioneves112',
-    database:'chinook'
-});
+    passowrd:'Antonioneves112',
+    database:'projeto'
+})
 
-//FAZ PING A BASE DE DADOS PARA VERIFICAR SE EXISTEM ERROS 
+//PING BASE DE DADOS PARA VERIFICAR SE EXISTEM EXCEPTION ERRORS
+pool.getConnection((err,connection)=>{
+    if(err){
+        if(err.code === 'PROTOCOL_CONNECTION_LOST'){
+            console.error('Aconexão com a base de dados foi fechada')
+        }
+        if(err.code === 'ER_CON_COUNT_ERROR'){
+            console.error('Base de dados tem muitas conexões')
+        }
+        if (err.code === 'ECONNREFUSED'){
+            console.error('Ligação com a base de dados foi rejeitada')
+        }
 
 
+    }
+
+    if (connection) connection.release()
+    return 
+})
+
+//PROMISE PARA NODE:JS ASYNC/AWAIT
+pool.query = util.promisify(pool.query)
+
+module.exports = pool
